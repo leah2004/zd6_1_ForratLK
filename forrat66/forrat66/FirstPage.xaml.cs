eb4b2565d756059b9e4a987f976305f9b4c169fa
leah2004 <ayadeer36@gmail.com>
2024-12-02 12:07:20 +0500

    Добавьте файлы проекта.

---
+++ forrat66/forrat66/FirstPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace forrat66
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
